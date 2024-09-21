a898ff4262ca38dc659f618be37dbdaaf51ba4ab
kafiahmad06 <khavie.com@gmail.com>
2024-09-21 01:15:26 -0700

    latihan coding 21 september 2024 bismillah masuk surga jalu viip

---
+++ main.py
@@ -0,0 +1,20 @@
+nilai = int(input("masukan nilai: "))
+
+if (nilai > 80): 
+    print("selamat anda lulus")
+elif (nilai > 90 and nilai <100):
+    print("selamata anada lulus denagan nilai muaskan")
+else:
+    print("anda masih belum lulus tes, semangat dan coba lagi")
+
+# for loop
+for i in range (1, 10,1):
+    print(i)
+    print(helo world)
+
+#while loop 
+angka = 10
+
+while(angka > 1):
+    print(angka)
+    angka -= 1 # angka = angka - 1
